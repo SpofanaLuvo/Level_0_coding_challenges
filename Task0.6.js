8b149de54d1615ddfe9ba5101d94630a2d4092e7
spofanaluvo <spofanaluvo@gmail.com>
2021-04-20 23:30:40 +0200

    Bug fixed

--- Task0.6.js
+++ Task0.6.js
@@ -1,15 +1,11 @@
 function getMaximum(){
-    var minValue = 0;
-    var maxValue = 0;
+    var minValue = Math.min(...arguments);
+    var maxValue = minValue;
     for (var i=0;i<arguments.length;i++){
-        if(arguments[i]<minValue){
-            minValue = arguments[i];
-        }
         if (arguments[i]>maxValue){
             maxValue=arguments[i];
         }
     }
-  
     return maxValue;
 }
-console.log(getMaximum(1,22,3,2));
+console.log(getMaximum(-1,-22,-3,2));
